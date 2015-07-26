1977d478e6a5a1e7f637c4811a42d375ebfd32b4
Felix Beuster <felix.beuster@beusterse.de>
2015-07-26 15:01:51 -0400

    fix FileLoader UTF-8 handling for JSON

--- classes/FileLoader.php
+++ classes/FileLoader.php
@@ -7,10 +7,23 @@ class FileLoader {
   }
 
   public static function loadJson($path, $as_array) {
-    $file_content = file_get_contents($path);
+    $file_content = FileLoader::file_get_contents_utf8($path);
     return json_decode($file_content, $as_array);
   }
 
+  public static function file_get_contents_utf8($path) {
+    $file_content = file_get_contents($path);
+
+    if (substr($file_content, 0, 3) == pack("CCC", 0xEF, 0xBB, 0xBF)) {
+      $file_content = substr($file_content, 3);
+    }
+
+    if ( !mb_check_encoding($file_content, 'UTF-8') ) {
+      $file_content = mb_convert_encoding($file_content, 'UTF-8');
+    }
+
+    return $file_content;
+  }
 }
 
 ?>
