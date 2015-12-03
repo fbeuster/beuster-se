0981efa00d3e767e496ffc4d9e010fd0dada8165
Felix Beuster <felix.beuster@beusterse.de>
2015-12-03 23:26:34 +0100

    add base_path option for config loader

--- classes/Config.php
+++ classes/Config.php
@@ -2,22 +2,24 @@
 
 class Config {
 
+  private $base_path;
   private $config;
   private static $instance;
   private static $key = '314159265';
 
-  public static function getConfig() {
+  public static function getConfig($base_path = '') {
     if(!self::$instance) {
-      self::$instance = new Config(self::$key);
+      self::$instance = new Config(self::$key, $base_path);
     }
     return self::$instance;
   }
 
-  public function __construct($key) {
+  public function __construct($key, $base_path = '') {
     if($key !== self::$key)
       throw new InvalidArgumentException( 'Private constructor!' );
 
-    $this->system_config  = FileLoader::loadIni('system/config.ini');
+    $this->base_path      = $base_path;
+    $this->system_config  = FileLoader::loadIni($this->base_path . 'system/config.ini');
     $this->user_config    = $this->loadUserConfig();
     $this->config         = array_merge($this->system_config, $this->user_config);
   }
@@ -35,8 +37,8 @@ class Config {
   }
 
   private function loadUserConfig() {
-    if(file_exists('user/config.ini')) {
-      return FileLoader::loadIni('user/config.ini');
+    if(file_exists($this->base_path . 'user/config.ini')) {
+      return FileLoader::loadIni($this->base_path . 'user/config.ini');
     }
 
     throw new Exception( 'No configuration file found.' );
