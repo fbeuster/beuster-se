7703ca7e15d49c428662474fb3f07cd8ebf441a1
Felix Beuster <felix.beuster@beusterse.de>
2015-12-05 23:05:22 +0100

    styles and localization for database page

--- setup/database.php
+++ setup/database.php
@@ -2,22 +2,21 @@
 
 <form action="index.php?step=database" method="post" class="<?php echo $form->getErrorClass(); ?>">
   <fieldset>
+    <legend><?php echo I18n::t("setup.database.legend"); ?></legend>
+    <div class="progress">
+      <div class="bar w20">&nbsp;</div>
+    </div>
     <?php $form->showMessages(); ?>
-    <span><a href="index.php?step=welcome">Back</a></span>
-    <br>
-    <legend>database</legend>
-    <label for="db_host">db_host</label>
+    <span class="nav_button"><a href="index.php?step=welcome"><?php echo I18n::t("setup.database.back_link"); ?></a></span>
+    <label for="db_host"><?php echo I18n::t("setup.database.label_db_host"); ?></label>
     <?php $form->textField('db_host', 'db_host', SetupHelper::getFieldOptions('db_host')); ?>
-    <br>
-    <label for="db_name">db_name</label>
+    <label for="db_name"><?php echo I18n::t("setup.database.label_db_name"); ?></label>
     <?php $form->textField('db_name', 'db_name', SetupHelper::getFieldOptions('db_name')); ?>
-    <br>
-    <label for="db_user">db_user</label>
+    <label for="db_user"><?php echo I18n::t("setup.database.label_db_user"); ?></label>
     <?php $form->textField('db_user', 'db_user'); ?>
-    <br>
-    <label for="db_pass">db_pass</label>
+    <label for="db_pass"><?php echo I18n::t("setup.database.label_db_pass"); ?></label>
     <?php $form->passwordField('db_pass', 'db_pass'); ?>
-    <br>
-    <input type="submit" value="Save and next">
+    <hr>
+    <input type="submit" value="<?php echo I18n::t("setup.database.submit"); ?>" class="nav_button">
   </fieldset>
 </form>
