7484231fc50ddbd66a1ecd3164a00d263083fc93
Felix Beuster <felix.beuster@beusterse.de>
2015-12-06 14:14:01 +0100

    add visibility toggle for dev server

--- setup/advanced.php
+++ setup/advanced.php
@@ -10,10 +10,12 @@
     <span class="nav_button"><a href="index.php?step=custom"><?php echo I18n::t("setup.advanced.back_link"); ?></a></span>
     <label for="devServer"><?php echo I18n::t("setup.advanced.label_dev_server"); ?></label>
     <?php $form->checkbox('devServer', 'devServer'); ?>
-    <label for="devServerAddress"><?php echo I18n::t("setup.advanced.label_dev_server_address"); ?></label>
-    <?php $form->textField('devServerAddress', 'devServerAddress'); ?>
-    <label for="remoteServerAddress"><?php echo I18n::t("setup.advanced.label_remote_server_address"); ?></label>
-    <?php $form->textField('remoteServerAddress', 'remoteServerAddress'); ?>
+    <div class="dev_server">
+      <label for="devServerAddress"><?php echo I18n::t("setup.advanced.label_dev_server_address"); ?></label>
+      <?php $form->textField('devServerAddress', 'devServerAddress'); ?>
+      <label for="remoteServerAddress"><?php echo I18n::t("setup.advanced.label_remote_server_address"); ?></label>
+      <?php $form->textField('remoteServerAddress', 'remoteServerAddress'); ?>
+    </div>
     <hr>
     <input type="submit" value="<?php echo I18n::t("setup.advanced.submit"); ?>" class="nav_button">
   </fieldset>
