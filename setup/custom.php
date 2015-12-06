9d3fd5bfbefb0e85e3e8b0bd8a90b83e5ea63463
Felix Beuster <felix.beuster@beusterse.de>
2015-12-06 13:47:15 +0100

    styles and localization for custom page

--- setup/custom.php
+++ setup/custom.php
@@ -2,27 +2,28 @@
 
 <form action="index.php?step=custom" method="post">
   <fieldset>
+    <legend><?php echo I18n::t("setup.custom.legend"); ?></legend>
+    <div class="progress">
+      <div class="bar w80">&nbsp;</div>
+    </div>
     <?php $form->showMessages(); ?>
-    <span><a href="index.php?step=admin_user">Back</a></span>
-    <br>
-    <legend>customization</legend>
+    <span class="nav_button"><a href="index.php?step=admin_user"><?php echo I18n::t("setup.custom.back_link"); ?></a></span>
 
+    <span class="section_title"><?php echo I18n::t("setup.custom.title_themes"); ?></span>
     <?php
       foreach (SetupHelper::getAvailableThemes() as $theme) {
         $form->label('theme_'.$theme, $theme);
         $form->radioButton('theme_'.$theme, 'theme', array('value' => $theme));
       }
     ?>
-    <br>
-    <label for="language">language</label>
+    <hr class="separator">
+    <label for="language"><?php echo I18n::t("setup.custom.label_language"); ?></label>
     <?php $form->selectField('language', 'language', SetupHelper::getAvailableLanguages(), 'en', SetupHelper::getFieldOptions('language')); ?>
-    <br>
-    <label for="timezone">timezone</label>
+    <label for="timezone"><?php echo I18n::t("setup.custom.label_timezone"); ?></label>
     <?php $form->selectField('timezone', 'timezone', SetupHelper::getAvailableTimezones(), SetupHelper::getTimezone(), SetupHelper::getFieldOptions('timezone')); ?>
-    <br>
-    <label for="rss_path">rss_path</label>
+    <label for="rss_path"><?php echo I18n::t("setup.custom.label_rss_path"); ?></label>
     <?php $form->textField('rss_path', 'rss_path', SetupHelper::getFieldOptions('rss_path', '/rss.xml')); ?>
-    <br>
-    <input type="submit" value="Save and next">
+    <hr>
+    <input type="submit" value="<?php echo I18n::t("setup.custom.submit"); ?>" class="nav_button">
   </fieldset>
 </form>
