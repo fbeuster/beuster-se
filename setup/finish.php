94a7fbfe5861d90571bb00351d15d5e754280375
Felix Beuster <felix.beuster@beusterse.de>
2015-12-06 17:19:32 +0100

    styles and localization for finish page

--- setup/finish.php
+++ setup/finish.php
@@ -1,2 +1,6 @@
 
-  <h1>done!</h1>
+<?php $link = '<a href="..">' . I18n::t("setup.finish.link") . '</a>'; ?>
+
+<h2><?php echo I18n::t("setup.finish.title"); ?></h2>
+<p><?php echo I18n::t("setup.finish.info", $link); ?></p>
+<hr>
