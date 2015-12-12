36b942bee0d960d94941988fa3d8e99c2d03bd42
Felix Beuster <felix.beuster@beusterse.de>
2015-12-12 17:48:28 +0100

    add missing todos in custom handler

--- classes/SetupHandler.php
+++ classes/SetupHandler.php
@@ -96,36 +96,42 @@ class SetupHandler {
   }
 
   private function handleCustom() {
-    $config_file = '../user/config.ini';
-    if (!file_exists($config_file)) {
+    $base_path    = '../';
+    $config_file  = 'user/config.ini';
+    $local_file   = 'user/local.php';
+
+    if (!file_exists($base_path . $config_file)) {
       $data = array(
                 "language = \"" . $_POST['language'] . "\"\n",
                 "theme = \"" . $_POST['theme'] . "\"\n"
               );
 
-      $output = file_put_contents($config_file, implode('', $data));
+      $output = file_put_contents($base_path . $config_file, implode('', $data));
 
       if ($output === false) {
+        $this->addMessage(I18n::t('setup.custom.file_write_error', $config_file));
         $this->error = true;
       }
     } else {
-      # todo handle existing file
+      $this->addMessage(I18n::t('setup.custom.file_missing', $config_file));
+      $this->error = true:
     }
 
-    $local_file = '../user/local.php';
-    if (file_exists($local_file)) {
+    if (file_exists($base_path . $local_file)) {
       $data = array(
                 "date_default_timezone_get('" . $_POST['timezone'] . "');\n",
                 "\$rssFeedPath = '" . $_POST['rss_path'] . "';\n"
               );
-      $output = file_put_contents($local_file, implode('', $data), FILE_APPEND);
+      $output = file_put_contents($base_path . $local_file, implode('', $data), FILE_APPEND);
 
       if ($output === false) {
+        $this->addMessage(I18n::t('setup.custom.file_write_error', $local_file));
         $this->error = true;
       }
 
     } else {
-      # todo handle missing file
+      $this->addMessage(I18n::t('setup.custom.file_missing', $local_file));
+      $this->error = true;
     }
   }
 
