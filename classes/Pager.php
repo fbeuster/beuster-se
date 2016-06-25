64948ec083cfb6491a9283d25c384c2d7529250f
Felix Beuster <felix.beuster@beusterse.de>
2016-06-25 22:44:56 +0200

    add new pager class

---
+++ classes/Pager.php
@@ -0,0 +1,112 @@
+<?php
+
+class Pager {
+
+  const VISIBLE_PAGES = 5;
+
+  private $current_page;
+  private $destination;
+  private $make_full_list;
+  private $next_text = null;
+  private $prev_text = null;
+  private $total_pages;
+
+  public function __construct($full_list, $total_pages, $current_page, $destination) {
+    $this->current_page   = $current_page;
+    $this->destination    = $destination;
+    $this->make_full_list = $full_list;
+    $this->total_pages    = $total_pages;
+  }
+
+  private function getFullPagesList() {
+    $ret = array();
+    $i = 1;
+
+    for($i; $i <= $this->total_pages; $i++) {
+      if ($i == $this->current_page) {
+        $page_active = true;
+        $page_href = "#";
+
+      } else {
+        $page_active = false;
+        $page_href = $this->destination . $i;
+      }
+      $ret[] = array($i, $page_href, $page_active);
+    }
+
+    return $ret;
+  }
+
+  public function getList() {
+    if ($this->next_text == null) {
+      $this->next_text = I18n::t('pager.next_text');
+      $this->prev_text = I18n::t('pager.prev_text');
+    }
+    $ret = array();
+
+    # prev
+    if ($this->current_page == 1) {
+      $prev_active = false;
+      $prev_href = '#';
+
+    } else {
+      $prev_active = true;
+      $prev_href = $this->destination.($this->current_page - 1);
+    }
+    $ret[] = array( $this->prev_text, $prev_href, $prev_active);
+
+    # pages
+    if ($this->make_full_list) {
+      $ret = array_merge($ret, $this->getFullPagesList());
+
+    } else {
+      if ($this->total_pages < Pager::VISIBLE_PAGES) {
+        $ret = array_merge($ret, $this->getFullPagesList());
+
+      } else {
+        $i = 1;
+
+        for($i; $i <= $this->total_pages; $i++) {
+          if ($i == $this->current_page) {
+            $page_active = true;
+            $page_href = "#";
+            $page_text = $i;
+
+          } elseif ($i == 1 || $i == $this->total_pages ||
+                    $i == $this->current_page - 1 ||
+                    $i == $this->current_page + 1) {
+            $page_active = false;
+            $page_href = $this->destination . $i;
+            $page_text = $i;
+
+          } else {
+            $page_active = false;
+            $page_href = '';
+            $page_text = '...';
+          }
+          $ret[] = array($page_text, $page_href, $page_active);
+        }
+      }
+    }
+
+    # next
+    if ($this->current_page == $this->total_pages) {
+      $next_active = false;
+      $next_href = '#';
+
+    } else {
+      $next_active = true;
+      $next_href = $this->destination.($this->current_page -1);
+    }
+    $ret[] = array( $this->next_text, $next_href, $next_active);
+
+    return $ret;
+  }
+
+  public function setNextPrevText($next_text, $prev_text) {
+    $this->next_text = $next_text;
+    $this->prev_text = $prev_text;
+  }
+}
+
+?>
--- theme/default/functions.php
+++ theme/default/functions.php
@@ -1,63 +1,50 @@
 <?php
 
   function genPager($anzPages, $currPage, $dest) {
-    $pagesToSee = 5;
-    $ret = '';
+    $ret    = '';
+    $pager  = new Pager(true, $anzPages, $currPage, $dest);
+    $pager->setNextPrevText('&nbsp;', '&nbsp;');
 
-    $i = 1;
-    $j = $anzPages;
-    $t = 1;
+    $list         = $pager->getList();
+    $last_element = count($list) - 1;
 
     $ret .= '<br class="clear">'."\r";
-    $ret .= '<!-- Pager ANFANG -->'."\r";
     $ret .= '<div id="pager">'."\r";
-    $ret .= ' <a href="';
-    if($currPage > 1) {
-      $ret .= $dest.($currPage - 1);
-    } else {
-      $ret .= '#';
-    }
-    $ret .= '" class="pArrows" id="pagerleft" title="Zurück">&nbsp;</a>'."\r";
+    $ret .= ' <a href="'.$list[0][1].'" class="pArrows" id="pagerleft" title="Zurück">'.$list[0][0].'</a>'."\r";
     $ret .= ' <div id="number"';
-    if($j < 5) {
+    if($anzPages < 5) {
       $ret .= ' style="text-align:center;"';
     }
     $ret .= '>'."\r";
-    $ret .= '  <ul class="pnr" id="pnr" style="width:'.($j*27-2).'px">'."\r";
-    for($i; $i <= $j; $i++) {
+    $ret .= '  <ul class="pnr" id="pnr" style="width:'.($anzPages*27-2).'px">'."\r";
+
+    foreach($list as $i => $item) {
+      if ($i == 0) continue;
+      if ($i == $last_element) continue;
+
+
       $link = '   <li';
-      if($i == $currPage) {
+      if($item[2]) {
         $link .= ' class="themecolor"';
       }
-      if($i == $t) {
+      if($i == 1) {
         $link .= ' style="display: block;"';
       }
-      $link .= '><a href="';
-      if($i == $currPage) {
-        $link .= '#';
-      } else {
-        $link .= $dest.$i;
-      }
-      $link .= '"';
-      if($i == $t && $i == $j) {
+      $link .= '><a href="'.$item[1].'"';
+      if($i == 1 && $i == $anzPages) {
         $link .= ' style="border: 0;"';
-      } else if($i == $t) {
+      } else if($i == 1) {
         $link .= ' style="border-left: 0;"';
-      } else if($i == $j) {
+      } else if($i == $anzPages) {
         $link .= ' style="border-right: 0;"';
       }
-      $link .= '>'.$i.'</a></li>'."\r";
+      $link .= '>'.$item[0].'</a></li>'."\r";
       $ret .= $link;
     }
+
     $ret .= '  </ul>'."\r";
     $ret .= ' </div>'."\r";
-    $ret .= ' <a href="';
-    if($currPage < $anzPages && $anzPages > 5) {
-      $ret .= $dest.($currPage + 1);
-    } else {
-      $ret .= '#';
-    }
-    $ret .= '" class="pArrows" id="pagerright" title="Weiter">&nbsp;</a>'."\r";
+    $ret .= ' <a href="'.$list[$last_element][1].'" class="pArrows" id="pagerright" title="Weiter">'.$list[$last_element][0].'</a>'."\r";
     $ret .= '</div>'."\r";
     return $ret;
   }
