a71c3870df9401bee2815eb416407b4a82e5a09f
Felix Beuster <felix.beuster@beusterse.de>
2016-07-03 01:51:56 +0200

    i18n editor buttons labels

--- system/views/editor.php
+++ system/views/editor.php
@@ -1,31 +1,31 @@
 <div class="bbDiv">
   <div class="bbSpan bbSpanFirst">
-    <button class="bbImg" type="button" title="Fett" id="btnbold">&nbsp;</button>
-    <button class="bbImg" type="button" title="Kursiv" id="btnitalic">&nbsp;</button>
-    <button class="bbImg" type="button" title="Unterstrichen" id="btnunderline">&nbsp;</button>
-    <button class="bbImg" type="button" title="Makiert" id="btnmark">&nbsp;</button>
-    <button class="bbImg" type="button" title="Als Gelöscht auszeichnen" id="btndel">&nbsp;</button>
-    <button class="bbImg" type="button" title="Eingefügt (nach Del.)" id="btnins">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.bold'); ?>" id="btnbold">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.italic'); ?>" id="btnitalic">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.underline'); ?>" id="btnunderline">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.mark'); ?>" id="btnmark">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.del'); ?>" id="btndel">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.ins'); ?>" id="btnins">&nbsp;</button>
   </div>
   <div class="bbSpan">
-    <button class="bbImg" type="button" title="Anführungsstriche" id="btnquote">&nbsp;</button>
-    <button class="bbImg" type="button" title="Inline-Zitat" id="btncite">&nbsp;</button>
-    <button class="bbImg" type="button" title="Blockzitat" id="btnbquote">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.quote'); ?>" id="btnquote">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.cite'); ?>" id="btncite">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.bquote'); ?>" id="btnbquote">&nbsp;</button>
   </div>
   <div class="bbSpan">
-    <button class="bbImg" type="button" title="Aufzählung" id="btnol">&nbsp;</button>
-    <button class="bbImg" type="button" title="Liste" id="btnul">&nbsp;</button>
-    <button class="bbImg" type="button" title="Listenelement" id="btnli">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.ol'); ?>" id="btnol">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.ul'); ?>" id="btnul">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.li'); ?>" id="btnli">&nbsp;</button>
   </div>
   <div class="bbSpan">
-    <button class="bbImg" type="button" title="Codebereich" id="btncode">&nbsp;</button>
-    <button class="bbImg" type="button" title="Neuer Absatz" id="btnpar">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.code'); ?>" id="btncode">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.par'); ?>" id="btnpar">&nbsp;</button>
   </div>
   <div class="bbSpan">
-    <button class="bbImg" type="button" title="Link einfügen" id="btnlink">&nbsp;</button>
-    <button class="bbImg" type="button" title="YouTube-Video einbetten" id="btnyt">&nbsp;</button>
-    <button class="bbImg" type="button" title="YouTube-Playlist einbinden" id="btnplay">&nbsp;</button>
-    <button class="bbImg" type="button" title="Amazon Affiliate" id="btnamazon">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.link'); ?>" id="btnlink">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.yt'); ?>" id="btnyt">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.play'); ?>" id="btnplay">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.amazon'); ?>" id="btnamazon">&nbsp;</button>
   </div>
   <div class="bbSpan">
     <button class="bbImg" type="button" title=":)" id="smsmile">&nbsp;</button>
@@ -34,8 +34,8 @@
     <button class="bbImg" type="button" title=";)" id="smone">&nbsp;</button>
   </div>
   <div class="bbSpan">
-    <button class="bbImg" type="button" title="Überschrift 2" id="btnuber2">&nbsp;</button>
-    <button class="bbImg" type="button" title="Überschrift 3" id="btnuber3">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.uber2'); ?>" id="btnuber2">&nbsp;</button>
+    <button class="bbImg" type="button" title="<?php echo I18n::t('editor.toolbar.label.uber3'); ?>" id="btnuber3">&nbsp;</button>
   </div>
 </div>
 <textarea name="<?php echo $args['name']; ?>"
