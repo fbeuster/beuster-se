bed421a47b922509841a5576aa226ed0ca6d3d92
Felix Beuster <felix.beuster@beusterse.de>
2016-07-10 13:07:56 +0200

    about mod now uses static page for output

--- includes/aboutMod.php
+++ includes/aboutMod.php
@@ -1,17 +1,23 @@
 <?php
   $a = array();
   $a['data'] = array();
+  $a['filename'] = 'static.php';
+
   if(!isset($_GET['aut'])) {
+    $a['data'] = I18n::t('aboutMod.no_username');
+
   } else {
-    $a['filename'] = 'aboutMod.php';
     $uName = stripslashes(trim($_GET['aut']));
 
-    $db = Database::getDB();
+    $db   = Database::getDB();
     $user = User::newFromName($uName);
+
     if(!$user->isLoaded() || !$user->isAdmin()) {
-      $a['data']['err'] = $uName;
+      $a['data'] = I18n::t('aboutMod.invalid_username', array($uName));
+
     } else {
-      $a['data']['about'] = $user;
+      $image = '<img src="/images/mods/' . $user->getName() . '.jpg" class="about">';
+      $a['data'] = $image . $user->buildInfo();
     }
   }
   return $a;
