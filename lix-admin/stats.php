7fa9ef3b3eda6b3f8dd531a0348e58c2e563104f
Felix Beuster <felix.beuster@beusterse.de>
2016-08-22 13:54:01 +0200

    translate and reindent admin stats

--- lix-admin/stats.php
+++ lix-admin/stats.php
@@ -1,118 +1,137 @@
 <?php
-    $a = array();
-    $user = User::newFromCookie();
-    if ($user && $user->isAdmin()) {
-        refreshCookies();
-        $a['filename'] = 'stats.php';
-        $a['data'] = array();
-        $db = Database::getDB()->getCon();
+  $a = array();
+  $user = User::newFromCookie();
 
-        // get top 10 article stats
-        $top = array();
-        $sql = "SELECT
-                    ID,
-                    Titel,
-                    Hits,
-                    Datum,
-                    TO_DAYS(NOW()) - TO_DAYS(Datum) AS TimeUp
-                FROM
-                    news
-                WHERE
-                    enable = 1 AND
-                    Datum < NOW()
-                GROUP BY
-                    ID
-                ORDER BY
-                    Hits DESC,
-                    Datum DESC
-                LIMIT
-                    0, 10";
-        if(!$stmt = $db->prepare($sql)) {
-            return $db->error;
-        }
-        if(!$stmt->execute()) {
-            return $result->error;
-        }
-        $stmt->bind_result($id, $newstitel, $hits, $date, $timeUp);
-        while($stmt->fetch()) {
-            $top[] = array(
-                        'Titel'     => Parser::parse($newstitel, Parser::TYPE_PREVIEW),
-                        'Link'      => '',
-                        'id'        => $id,
-                        'date'      => date("d.m.Y H:i", strtotime($date)),
-                        'hits'      => $hits,
-                        'hitsPerDay'=> number_format($hits / ($timeUp<1?1:$timeUp), 2, '.', ','));
-        }
-        $stmt->close();
-        foreach($top as $k => $v) {
-            $top[$k]['Link'] = getLink(getCatName(getNewsCat($v['id'])), $v['id'], $v['Titel']);
-        }
-        $a['data']['top'] = $top;
-        // get last 10 article stats
-        $last = array();
-        $sql = "SELECT
-                    ID,
-                    Titel,
-                    Hits,
-                    Datum,
-                    TO_DAYS(NOW()) - TO_DAYS(Datum) AS TimeUp
-                FROM
-                    news
-                WHERE
-                    enable = 1 AND
-                    Datum < NOW()
-                GROUP BY
-                    ID
-                ORDER BY
-                    Datum DESC,
-                    Hits DESC
-                LIMIT
-                    0, 10";
-        if(!$stmt = $db->prepare($sql)) {
-            return $db->error;
-        }
-        if(!$stmt->execute()) {
-            return $result->error;
-        }
-        $stmt->bind_result($id, $newstitel, $hits, $date, $timeUp);
-        while($stmt->fetch()) {
-            $last[] = array(
-                        'Titel'     => Parser::parse($newstitel, Parser::TYPE_PREVIEW),
-                        'Link'      => '',
-                        'id'        => $id,
-                        'date'      => date("d.m.Y H:i", strtotime($date)),
-                        'hits'      => $hits,
-                        'hitsPerDay'=> number_format($hits / ($timeUp<1?1:$timeUp), 2, '.', ','));
-        }
-        $stmt->close();
-        foreach($last as $k => $v) {
-            $last[$k]['Link'] = getLink(getCatName(getNewsCat($v['id'])), $v['id'], $v['Titel']);
-        }
-        $a['data']['last'] = $last;
-        $sql = "SELECT
-                    Name,
-                    downloads
-                FROM
-                    files";
-        if(!$stmt = $db->prepare($sql)) {
-            return $db->error;
-        }
-        if(!$stmt->execute()) {
-            return $stmt->error;
-        }
-        $stmt->bind_result($name, $downloads);
-        $down = array();
-        while($stmt->fetch()) {
-                $down[] = array(
-                            'name' => $name,
-                            'down' => $downloads);
-        }
-        $a['data']['down'] = $down;
-        $stmt->close();
-        return $a; // nicht Vergessen, sonst enthält $ret nur den Wert int(1)
-    } else if($user){
-        return showInfo('Sie haben hier keine Zugriffsrechte.', 'blog');
-    } else {
-        return showInfo('Sie sind nicht eingeloggt. <a href="/login" class="back">Erneut versuchen</a>', 'login');
+  if ($user && $user->isAdmin()) {
+    refreshCookies();
+    $a['filename'] = 'stats.php';
+    $a['data'] = array();
+    $db = Database::getDB()->getCon();
+
+    # get top 10 article statistics
+    $top = array();
+    $sql = "SELECT
+              ID,
+              Titel,
+              Hits,
+              Datum,
+              TO_DAYS(NOW()) - TO_DAYS(Datum) AS TimeUp
+            FROM
+              news
+            WHERE
+              enable = 1 AND
+              Datum < NOW()
+            GROUP BY
+              ID
+            ORDER BY
+              Hits DESC,
+              Datum DESC
+            LIMIT
+              0, 10";
+
+    if(!$stmt = $db->prepare($sql)) {
+      return $db->error;
     }
+
+    if(!$stmt->execute()) {
+      return $result->error;
+    }
+    $stmt->bind_result($id, $title, $hits, $date, $uptime);
+
+    while($stmt->fetch()) {
+      $top[] = array(
+                  'title'   => Parser::parse($title, Parser::TYPE_PREVIEW),
+                  'link'    => '',
+                  'id'      => $id,
+                  'date'    => date("d.m.Y H:i", strtotime($date)),
+                  'hits'    => $hits,
+                  'per_day' => number_format($hits / ($uptime < 1 ? 1 : $uptime), 2, '.', ','));
+    }
+    $stmt->close();
+
+    foreach($top as $k => $v) {
+      $top[$k]['link'] = getLink(getCatName(getNewsCat($v['id'])), $v['id'], $v['title']);
+    }
+    $a['data']['top'] = $top;
+
+    # get last 10 article statistics
+    $last = array();
+    $sql = "SELECT
+              ID,
+              Titel,
+              Hits,
+              Datum,
+              TO_DAYS(NOW()) - TO_DAYS(Datum) AS TimeUp
+            FROM
+              news
+            WHERE
+              enable = 1 AND
+              Datum < NOW()
+            GROUP BY
+              ID
+            ORDER BY
+              Datum DESC,
+              Hits DESC
+            LIMIT
+              0, 10";
+
+    if(!$stmt = $db->prepare($sql)) {
+      return $db->error;
+    }
+
+    if(!$stmt->execute()) {
+      return $result->error;
+    }
+    $stmt->bind_result($id, $title, $hits, $date, $uptime);
+
+    while($stmt->fetch()) {
+      $last[] = array(
+                  'title'   => Parser::parse($title, Parser::TYPE_PREVIEW),
+                  'link'    => '',
+                  'id'      => $id,
+                  'date'    => date("d.m.Y H:i", strtotime($date)),
+                  'hits'    => $hits,
+                  'per_day' => number_format($hits / ($uptime < 1 ? 1 : $uptime), 2, '.', ','));
+    }
+    $stmt->close();
+
+    foreach($last as $k => $v) {
+      $last[$k]['link'] = getLink(getCatName(getNewsCat($v['id'])), $v['id'], $v['title']);
+    }
+    $a['data']['last'] = $last;
+
+    # get download statistics
+    $sql = "SELECT
+              Name,
+              downloads
+            FROM
+              files";
+
+    if(!$stmt = $db->prepare($sql)) {
+      return $db->error;
+    }
+
+    if(!$stmt->execute()) {
+      return $stmt->error;
+    }
+    $stmt->bind_result($name, $downloads);
+
+    $down = array();
+    while($stmt->fetch()) {
+          $down[] = array(
+                      'name' => $name,
+                      'down' => $downloads);
+    }
+    $a['data']['down'] = $down;
+
+    $stmt->close();
+    return $a;
+
+  } else if ($user) {
+    return showInfo(I18n::t('admin.no_access'), 'blog');
+
+  } else {
+    $link = ' <a href="/login">'.I18n::t('admin.try_again').'</a>';
+    return showInfo(i18N::T('admin.not_logged_in').$link, 'login');
+  }
 ?>
--- system/views/admin/stats.php
+++ system/views/admin/stats.php
@@ -1,18 +1,18 @@
 
 <article>
-  <a href="/admin" class="back">&lt; Zurück zur Administration</a>
+  <a href="/admin" class="back"><?php echo I18n::t('admin.back_link'); ?></a>
   <section class="article">
-    <h1>Statistiken</h1>
+    <h1><?php echo I18n::t('admin.stats.label'); ?></h1>
 
-    <h2>Artikelranking (Letzten 10 Artikel)</h2>
+    <h2><?php echo I18n::t('admin.stats.rankings_table.last_label'); ?></h2>
     <table class="newslist">
       <thead>
         <tr>
           <th class="smallNumber">#</th>
-          <th class="date">Datum</th>
-          <th class="bigNumber">Hits</th>
-          <th class="bigNumber">Hits/d</th>
-          <th>Artikel</th>
+          <th><?php echo I18n::t('admin.stats.rankings_table.header.article'); ?></th>
+          <th class="bigNumber"><?php echo I18n::t('admin.stats.rankings_table.header.hits'); ?></th>
+          <th class="bigNumber"><?php echo I18n::t('admin.stats.rankings_table.header.hits_per_day'); ?></th>
+          <th class="date"><?php echo I18n::t('admin.stats.rankings_table.header.date'); ?></th>
         </tr>
       </thead>
       <tbody>
@@ -22,10 +22,10 @@
           foreach($data['last'] as $entry) { ?>
         <tr>
           <td><?php echo ($i+1); ?></td>
-          <td><?php echo $entry['date']; ?></td>
+          <td><a href="<?php echo $entry['link']; ?>"><?php echo $entry['title']; ?></a></td>
           <td><?php echo $entry['hits']; ?></td>
-          <td><?php echo $entry['hitsPerDay']; ?></td>
-          <td><a href="<?php echo $entry['Link']; ?>"><?php echo $entry['Titel']; ?></a></td>
+          <td><?php echo $entry['per_day']; ?></td>
+          <td><?php echo $entry['date']; ?></td>
         </tr>
       <?php
             $i++;
@@ -34,15 +34,15 @@
       </tbody>
     </table>
 
-    <h2>Artikelranking (Top10 Aufrufe)</h2>
+    <h2><?php echo I18n::t('admin.stats.rankings_table.top_label'); ?></h2>
     <table class="newslist">
       <thead>
         <tr>
           <th class="smallNumber">#</th>
-          <th class="date">Datum</th>
-          <th class="bigNumber">Hits</th>
-          <th class="bigNumber">Hits/d</th>
-          <th>Artikel</th>
+          <th><?php echo I18n::t('admin.stats.rankings_table.header.article'); ?></th>
+          <th class="bigNumber"><?php echo I18n::t('admin.stats.rankings_table.header.hits'); ?></th>
+          <th class="bigNumber"><?php echo I18n::t('admin.stats.rankings_table.header.hits_per_day'); ?></th>
+          <th class="date"><?php echo I18n::t('admin.stats.rankings_table.header.date'); ?></th>
         </tr>
       </thead>
       <tbody>
@@ -52,10 +52,10 @@
           foreach($data['top'] as $entry) { ?>
         <tr>
           <td><?php echo ($i+1); ?></td>
-          <td><?php echo $entry['date']; ?></td>
+          <td><a href="<?php echo $entry['link']; ?>"><?php echo $entry['title']; ?></a></td>
           <td><?php echo $entry['hits']; ?></td>
-          <td><?php echo $entry['hitsPerDay']; ?></td>
-          <td><a href="<?php echo $entry['Link']; ?>"><?php echo $entry['Titel']; ?></a></td>
+          <td><?php echo $entry['per_day']; ?></td>
+          <td><?php echo $entry['date']; ?></td>
         </tr>
       <?php
             $i++;
@@ -64,12 +64,12 @@
       </tbody>
     </table>
 
-    <h2>Downloads</h2>
+    <h2><?php echo I18n::t('admin.stats.downloads_table.label'); ?></h2>
     <table class="newslist">
       <thead>
         <tr>
-          <th class="bigNumber">Downl.</th>
-          <th>Programm</th>
+          <th class="bigNumber"><?php echo I18n::t('admin.stats.downloads_table.header.downloads'); ?></th>
+          <th><?php echo I18n::t('admin.stats.downloads_table.header.file'); ?></th>
         </tr>
       </thead>
       <tbody>
@@ -89,5 +89,5 @@
     </table>
   </section>
 
-  <a href="/admin" class="back">&lt; Zurück zur Administration</a>
+  <a href="/admin" class="back"><?php echo I18n::t('admin.back_link'); ?></a>
 </article>
