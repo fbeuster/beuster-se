be393c53cade742eb99fdd10f94d583aa965a483
Felix Beuster <felix.beuster@beusterse.de>
2016-08-22 12:56:30 +0200

    translate and re-indent admin file

--- lix-admin/admin.php
+++ lix-admin/admin.php
@@ -1,16 +1,19 @@
 <?php
-    $a = array();
-    $user = User::newFromCookie();
-    if ($user && $user->isAdmin()) {
-        refreshCookies();
-        $a['filename'] = 'admin.php';
-        $a['data'] = array();
-        return $a; // nicht Vergessen, sonst enthält $ret nur den Wert int(1)
-    } else if($user){
-        return showInfo('Sie haben hier keine Zugriffsrechte.', 'blog');
-    } else {
-        $a['filename'] = 'login.php';
-        $a['data'] = array();
-        return $a; // nicht Vergessen, sonst enthält $ret nur den Wert int(1)
-    }
+  $a = array();
+  $user = User::newFromCookie();
+
+  if ($user && $user->isAdmin()) {
+    refreshCookies();
+    $a['filename'] = 'admin.php';
+    $a['data'] = array();
+    return $a;
+
+  } else if ($user) {
+    return showInfo(I18n::t('admin.no_access'), 'blog');
+
+  } else {
+    $a['filename'] = 'login.php';
+    $a['data'] = array();
+    return $a;
+  }
 ?>
