ff6176b9745bb746f415a30ed53eb25b67d2f89d
Felix Beuster <felix.beuster@beusterse.de>
2016-10-06 13:55:31 +0200

    reindent and localize new snippet

--- lix-admin/snippetnew.php
+++ lix-admin/snippetnew.php
@@ -1,32 +1,36 @@
 <?php
-  $a = array();
+  $a    = array();
   $user = User::newFromCookie();
+
   if ($user && $user->isAdmin()) {
     refreshCookies();
-    $a['filename'] = 'snippetnew.php';
-    $a['data'] = array();
-    $err = 0;
-    $db = Database::getDB();
+
+    $a['filename']  = 'snippetnew.php';
+    $a['data']      = array();
+
+    $err  = 0;
+    $db   = Database::getDB();
 
     if ('POST' == $_SERVER['REQUEST_METHOD']) {
-      $content = Parser::parse($_POST['content'], Parser::TYPE_NEW);
-      $name = trim($_POST['name']);
+      $content  = Parser::parse($_POST['content'], Parser::TYPE_NEW);
+      $name     = trim($_POST['name']);
 
-      $eRet = array(  'content' => $content,
-                      'name'    => $name);
-      if('' == $name || '' == $content) {
+      $eRet     = array(  'content' => $content,
+                          'name'    => $name);
+
+      if ('' == $name || '' == $content) {
         # empty name or content
         $err = 1;
 
-      } else if(strlen($name) > 20) {
+      } else if (strlen($name) > 20) {
         # too long
         $err = 2;
 
-      } else if(!preg_match('#^[A-Za-z0-9]*$#', $name)) {
+      } else if (!preg_match('#^[A-Za-z0-9]*$#', $name)) {
         # invalid characters
         $err = 3;
 
-      } else if(Snippet::exists($name)) {
+      } else if (Snippet::exists($name)) {
         # already exists
         $err = 4;
 
@@ -39,18 +43,23 @@
         $res    = $db->insert('snippets', $fields, $values);
       }
 
-      if($err != 0) {
-          $eRet['t'] = $err;
-          $a['data']['fe'] = $eRet;
+      if ($err != 0) {
+        $eRet['t']        = $err;
+        $a['data']['fe']  = $eRet;
+
       } else {
-          return showInfo('Das Snippet wurde hinzugefügt. <br /><a href="/admin" class="back">Zurück zur Administration</a>', 'admin');
+        $link = ' <br /><a href="/admin">'.I18n::t('admin.back_link').'</a>';
+        return showInfo(I18n::t('admin.snippet.new.success').$link, 'admin');
       }
     }
 
-    return $a; // nicht Vergessen, sonst enthält $ret nur den Wert int(1)
-  } else if($user){
-    return showInfo('Sie haben hier keine Zugriffsrechte.', 'blog');
+    return $a;
+
+  } else if ($user) {
+    return showInfo(I18n::t('admin.no_access'), 'blog');
+
   } else {
-    return showInfo('Sie sind nicht eingeloggt. <a href="/login" class="back">Erneut versuchen</a>', 'login');
+    $link = ' <a href="/login">'.I18n::t('admin.try_again').'</a>';
+    return showInfo(I18n::t('admin.not_logged_in').$link, 'login');
   }
 ?>
--- system/views/admin/snippetnew.php
+++ system/views/admin/snippetnew.php
@@ -1,6 +1,6 @@
 
 <article>
-  <a href="/admin" class="back">&lt; Zurück zur Administration</a>
+  <a href="/admin" class="back"><?php echo I18n::t('admin.back_link'); ?></a>
   <section class="article">
     <?php if(isset($data['fe'])){ ?>
       <p class="alert">
@@ -9,22 +9,22 @@
     <?php } ?>
     <form action="/snippetnew" method="post" enctype="multipart/form-data" class="userform articleform">
       <fieldset>
-        <legend>Neues Snippet erstellen</legend>
+        <legend><?php echo I18n::t('admin.snippet.new.label'); ?></legend>
         <label class="required long">
-          <span>Name</span>
-          <input type="text" name="name" title="Name of the Snippet" placeholder="Name of the snippet">
+          <span><?php echo I18n::t('admin.snippet.new.name.label'); ?></span>
+          <input type="text" name="name" title="<?php echo I18n::t('admin.snippet.new.name.placeholder'); ?>" placeholder="<?php echo I18n::t('admin.snippet.new.name.placeholder'); ?>">
         </label>
         <label class="required long">
-          <span>Inhalt</span>
+          <span><?php echo I18n::t('admin.snippet.new.content_label'); ?></span>
           <?php
             $content  = isset($data['fe']['inhalt']) ? $data['fe']['inhalt'] : '';
             $editor   = new Editor('newsinhalt', 'content', $content);
             $editor->show();
           ?>
         </label>
-        <input type="submit" name="formaction" value="Snippet anlegen" />
+        <input type="submit" name="formaction" value="<?php echo I18n::t('admin.snippet.new.submit'); ?>" />
       </fieldset>
     </form>
   </section>
-  <a href="/admin" class="back">&lt; Zurück zur Administration</a>
+  <a href="/admin" class="back"><?php echo I18n::t('admin.back_link'); ?></a>
 </article>
