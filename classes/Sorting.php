9cbe7d31cb33dd2f565e57c484fee58bacb5cc3c
Felix Beuster <felix.beuster@beusterse.de>
2017-02-09 22:18:49 +0100

    remove deprecated download classes

--- classes/Sorting.php
+++ classes/Sorting.php
@@ -41,32 +41,6 @@ class Sorting {
 		return $a->getDate() < $b->getDate() ? 1 : -1;
 	}
 
-	/**
-	 * Sorts two DownloadFile by name, ascending oder.
-	 *
-	 * @param DownloadFile $a first item
-	 * @param DownloadFile $b second item
-	 * @return int
-	 */
-	public static function downloadFileByNameAsc($a, $b) {
-		$a = strtolower($a->getName());
-		$b = strtolower($b->getName());
-		return strcmp($a, $b);
-	}
-
-	/**
-	 * Sorts two DownloadFile by name, descending oder.
-	 *
-	 * @param DownloadFile $a first item
-	 * @param DownloadFile $b second item
-	 * @return int
-	 */
-	public static function downloadFileByNameDesc($a, $b) {
-		$a = strtolower($a->getName());
-		$b = strtolower($b->getName());
-		return -1 * strcmp($a, $b);
-	}
-
 	public static function searchResultScoreDesc($a, $b) {
 		if (!isset($a['score'], $b['score'])) {
 			return 0;
