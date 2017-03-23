5e739cf2d451c96e1c0ba3aaf81c40d363aa64f7
Felix Beuster <felix.beuster@beusterse.de>
2017-03-24 00:23:45 +0100

    fix empty aside box

--- theme/beuster-se-2016/htmlaside.php
+++ theme/beuster-se-2016/htmlaside.php
@@ -1,14 +1,15 @@
-    <aside>
-      <?php
-      $page = Lixter::getLix()->getPage();
+<?php
+  $page = Lixter::getLix()->getPage();
 
-      if($page->getType() == Page::ARTICLE_PAGE) {
-        echo '<p class="amazon_disclaimer">';
-        I18n::e('article.amazon_disclaimer');
-        echo '</p>';
-      }
+  if ($page->getType() == Page::ARTICLE_PAGE) { ?>
+  <aside>
+    <?php
+      echo '<p class="amazon_disclaimer">';
+      I18n::e('article.amazon_disclaimer');
+      echo '</p>';
 
       // this integration will follow at a later point
       // echo moduleArchive();
- ?>
-    </aside>
+    ?>
+  </aside>
+<?php } ?>
