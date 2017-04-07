f0f344bafce1a4400d251fcc0149439d3b0da35b
Felix Beuster <felix.beuster@beusterse.de>
2017-04-07 23:27:05 +0200

    refactor archive list generation to one single db call

--- classes/SidebarModuleArchive.php
+++ classes/SidebarModuleArchive.php
@@ -17,34 +17,75 @@
     }
 
     private function generateList() {
+      $counts   = '';
+      $db       = Database::getDB()->getCon();
       $max_year = (int) date("Y");
       $min_year = 2010;
 
       for ($year = $max_year; $year >= $min_year; $year--) {
-
-        if (articlesInDate($year) === 0) {
-          continue;
+        for ($month = 12; $month >= 1; $month--) {
+          $counts     .= 'COUNT(case when YEAR(`Datum`) = ' . $year . ' and MONTH(`Datum`) = ' . $month . ' then ID end) AS `' . $month . '-' . $year .'`, ';
         }
+      }
 
+      $counts = substr($counts, 0, strlen($counts) - 2);
+      $sql    = "SELECT $counts FROM news";
+
+      # todo improve error handling, maybe just logging and leave method?
+      if (!$result = $db->prepare($sql)) {
+          $this->list['error'] = $db->error;
+      }
+
+      if (!$result->execute()) {
+          $this->list['error'] = $result->error;
+      }
+
+      $rs   = array();
+      $meta = $result->result_metadata();
+
+      while ($f = $meta->fetch_field()) {
+        $var      = $f->name;
+        $$var     = null;
+        $rs[$var] = &$$var;
+      }
+
+      call_user_func_array(array($result, 'bind_result'), $rs);
+
+      if (!$result->fetch()) {
+          $return = $result->error;
+      }
+
+      for ($year = $max_year; $year >= $min_year; $year--) {
         $month_list = array();
+
         for ($month = 12; $month >= 1; $month--) {
 
-          $numberMonth = articlesInDate($year, $month);
+          $numberMonth = $rs[$month . '-' . $year];
           if ($numberMonth === 0) {
             continue;
           }
 
-          $month_str  = '<a href="/'.$year.'/'.$month.'">';
-          $month_str  .= I18n::t('datetime.month.' . $month);
-          $month_str  .= ' <span class="number" style="color: #999999;">('.$numberMonth.')</span>';
-          $month_str  .= '</a>';
+          $month_str    = '<a href="/'.$year.'/'.$month.'">';
+          $month_str    .= I18n::t('datetime.month.' . $month);
+          $month_str    .= ' <span class="number" style="color: #999999;">';
+          $month_str    .= '('.$numberMonth.')';
+          $month_str    .= '</span>';
+          $month_str    .= '</a>';
           $month_list[] = $month_str;
         }
 
-        $year_title = '<span class="articleArchiveYear" style="cursor: pointer;">'.$year."</span>";
+        if (empty($month_list)) {
+          continue;
+        }
+
+        $year_title = '<span class="articleArchiveYear" style="cursor: pointer;">';
+        $year_title .= $year;
+        $year_title .= '</span>';
 
         $this->list[$year_title] = $month_list;
       }
+
+      $result->close();
     }
 
     public static function html() {
