07380bf1afef48f5864621a52dc39160804b6681
Felix Beuster <felix.beuster@beusterse.de>
2017-09-22 02:32:58 +0200

    add schema selection to LinkBuilder class

--- classes/LinkBuilder.php
+++ classes/LinkBuilder.php
@@ -1,6 +1,11 @@
 <?php
 
   class LinkBuilder {
+    # schemas
+    const CUSTOM_SCHEMA     = 0;
+    const DEFAULT_SCHEMA    = 1;
+    const PARAMETER_SCHEMA  = 2;
+
     # article schemas
     const DEFAULT_ARTICLE_SCHEMA    = '/#id#/#category#/#title#';
     const PARAMETER_ARTICLE_SCHEMA  = '/index.php?p=blog&n=#id#';
@@ -9,15 +14,18 @@
     const DEFAULT_CATEGORY_SCHEMA = '/#name#';
     const PARAMETER_CATEGORY_SCHEMA = '/index.php?p=#name#';
 
+    private $article_link;
+    private $category_link;
+
     public function __construct() {
       # TODO
       # getting current schema from database
+
+      $this->setSchema();
     }
 
     public function makeArticleLink($id, $category, $title) {
-      $link = self::DEFAULT_ARTICLE_SCHEMA;
-
-      $link = str_replace('#id#',       $id,        $link);
+      $link = str_replace('#id#',       $id,        $this->article_link);
       $link = str_replace('#category#', $category,  $link);
       $link = str_replace('#title#',
                           $this->removeSpecialCharacters($title),
@@ -29,8 +37,7 @@
       $category_name = $this->replaceStrokes($category_name);
       $category_name = mb_strtolower($category_name, 'UTF-8');
 
-      $link = self::DEFAULT_CATEGORY_SCHEMA;
-      $link = str_replace('#name#', $category_name,  $link);
+      $link = str_replace('#name#', $category_name, $this->category_link);
 
       return $link;
     }
@@ -67,6 +74,26 @@
       $string = $this->replaceStrokes($string);
 
       return $this->replaceUmlaute($string);
+    }
+
+    private function setSchema() {
+      $selected_schema = self::DEFAULT_SCHEMA;
+
+      switch ($selected_schema) {
+        case self::CUSTOM_SCHEMA :
+          break;
+
+        case self::PARAMETER_SCHEMA :
+          $this->article_link   = self::PARAMETER_ARTICLE_SCHEMA;
+          $this->category_link  = self::PARAMETER_CATEGORY_SCHEMA;
+          break;
+
+        case self::DEFAULT_SCHEMA :
+        default :
+          $this->article_link   = self::DEFAULT_ARTICLE_SCHEMA;
+          $this->category_link  = self::DEFAULT_CATEGORY_SCHEMA;
+          break;
+      }
     }
   }
 
