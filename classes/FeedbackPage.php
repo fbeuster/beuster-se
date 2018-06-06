27c02babc60cff700db0aba0a5772ad45588a319
Felix Beuster <felix.beuster@beusterse.de>
2018-06-06 11:55:42 +0200

    fix contact page xss

--- classes/FeedbackPage.php
+++ classes/FeedbackPage.php
@@ -27,10 +27,10 @@ class FeedbackPage extends Page {
     if ($this->hadData()) {
       $this->errors = array();
       $this->values = array(
-        self::NAME_MAIL     => $_POST[self::NAME_MAIL],
-        self::NAME_MESSAGE  => $_POST[self::NAME_MESSAGE],
-        self::NAME_NAME     => $_POST[self::NAME_NAME],
-        self::NAME_PAGE     => $_POST[self::NAME_PAGE]
+        self::NAME_MAIL     =>                trim( $_POST[self::NAME_MAIL]     ),
+        self::NAME_MESSAGE  => htmlentities(  trim( $_POST[self::NAME_MESSAGE]  ) ),
+        self::NAME_NAME     => htmlentities(  trim( $_POST[self::NAME_NAME]     ) ),
+        self::NAME_PAGE     => htmlentities(  trim( $_POST[self::NAME_PAGE]     ) ),
       );
       $this->validate();
 
@@ -141,16 +141,16 @@ class FeedbackPage extends Page {
   }
 
   private function validate() {
-    if (!isset($_POST[self::NAME_NAME]) || trim($_POST[self::NAME_NAME]) == '') {
+    if ($this->values[self::NAME_NAME] == '') {
       $this->errors[self::NAME_NAME] = I18n::t('general_form.errors.invalid_name');
     }
 
-    if (!isset($_POST[self::NAME_MAIL]) || trim($_POST[self::NAME_MAIL]) == '' ||
+    if ($this->values[self::NAME_MAIL] == '' ||
         !checkMail($_POST[self::NAME_MAIL])) {
       $this->errors[self::NAME_MAIL] = I18n::t('general_form.errors.invalid_mail');
     }
 
-    if (!isset($_POST[self::NAME_MESSAGE]) || trim($_POST[self::NAME_MESSAGE]) == '') {
+    if ($this->values[self::NAME_MESSAGE] == '') {
       $this->errors[self::NAME_MESSAGE] = I18n::t('general_form.errors.invalid_message');
     }
 
